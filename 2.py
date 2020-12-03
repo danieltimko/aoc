566355601663e5e51f457bb04d1792e32900141f
Daniel Timko <xtimko@fi.muni.cz>
2020-12-03 06:43:38 +0100

    days 1-3

--- 2.py
+++ 2.py
@@ -3,7 +3,19 @@ def xor(a, b):
     return (a and not b) or (not a and b)
 
 
-def is_valid(entry):
+def is_valid1(entry):
+    min = int(entry[0].split('-')[0])
+    max = int(entry[0].split('-')[1].split(' ')[0])
+    letter = entry[0].split('-')[1].split(' ')[1]
+    password = entry[1][1:]
+    occurences = 0
+    for c in password:
+        if c == letter:
+            occurences += 1
+    return min <= occurences <= max
+
+
+def is_valid2(entry):
     min = int(entry[0].split('-')[0])
     max = int(entry[0].split('-')[1].split(' ')[0])
     letter = entry[0].split('-')[1].split(' ')[1]
@@ -18,7 +30,7 @@ def run():
         lines = list(map(lambda line: (line.strip().split(':')), file.readlines()))
         count = 0
         for line in lines:
-            if is_valid(line):
+            if is_valid1(line):
                 count += 1
         print(count)
 

eb055a9a19dd6a9702de189d0e3faeebd92155ab
Daniel Timko <daniel.timko4@gmail.com>
2021-12-07 09:57:21 +0100

    Add day 7

--- 2021/07.py
+++ 2021/07.py
@@ -1,19 +1,9 @@
 
-def nsum(n):
-    return n*(n+1)//2
-
-
-def calc_fuel(arr, task):
-    return min(
-        sum(abs(x-y) if task == 1 else nsum(abs(x-y)) for y in arr)
-        for x in arr
-    )
-
-
 def run():
     with open('input') as file:
         arr = list(map(int, file.readlines()[0].split(',')))
-        print(calc_fuel(arr, task=1), calc_fuel(arr, task=2))
+        print(min(sum(abs(x-y) for y in arr) for x in arr),
+              min(sum(abs(x-y)*(abs(x-y)+1)//2 for y in arr) for x in arr))
 
 
 run()

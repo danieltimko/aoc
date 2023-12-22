c680db5d999970d7c211e86c81abc2c9eeaa15ef
Daniel Timko <daniel.timko4@gmail.com>
2023-12-22 14:05:47 +0100

    Add day 21

--- 2023/21.py
+++ 2023/21.py
@@ -11,13 +11,15 @@ def find_start(grid):
                 return r, c
 
 
-def bfs(grid, start, task=1, odd=True, limit=0):
+def bfs(grid, start, task=1, odd=True, limit=None):
+    if limit is not None:
+        assert limit % 2 == odd
     q = deque([(start, 0)])
     visited = set()
     reachable = set()
     while q:
         (r, c), i = q.popleft()
-        if (r, c) in visited:  # TODO move
+        if (r, c) in visited:
             continue
         visited.add((r, c))
         if task == 1:
@@ -26,136 +28,78 @@ def bfs(grid, start, task=1, odd=True, limit=0):
             if i % 2 == 0:
                 reachable.add((r, c))
         else:
-            if limit and i > limit:
+            if limit is not None and i > limit:
                 break
             if (odd and i % 2 == 1) or (not odd and i % 2 == 0):
                 reachable.add((r, c))
         for adj in [(r, c-1), (r, c+1), (r-1, c), (r+1, c)]:
             if adj[0] not in range(len(grid)) or adj[1] not in range(len(grid[0])):
                 continue
-            if grid[adj[0]][adj[1]] in '.S' and adj not in visited:
+            if grid[adj[0]][adj[1]] in '.S':
                 q.append((adj, i+1))
     return len(reachable), reachable
 
 
-def count_dots(grid):
-    return sum(row.count('.') for row in grid)
-
-
-def get_reachable_nodes(grid, start):
-    q = deque([(start, 0)])
-    visited = set()
-    while q:
-        (r, c), i = q.popleft()
-        if (r, c) in visited:  # TODO move
-            continue
-        visited.add((r, c))
-        for adj in [(r, c - 1), (r, c + 1), (r - 1, c), (r + 1, c)]:
-            if adj[0] not in range(len(grid)) or adj[1] not in range(len(grid[0])):
-                continue
-            if grid[adj[0]][adj[1]] in '.S' and adj not in visited:
-                q.append((adj, i + 1))
-    return visited
-
-
 def task2(grid):
+    """
+    assumptions:
+    - grid size is odd
+    - start (S) is in the center
+    - center row/col and also edges are free of obstacles
+    """
     start = find_start(grid)
     n_steps = 26501365
-
-    # number of nodes reachable from specific node in odd/even number of steps
-    # subresults = {
-    #     from_position: len(get_reachable_nodes(grid, from_position))
-    #     for from_position in [
-    #         start,  # odd
-    #         (start[0], 0),  # even
-    #         (start[0], len(grid)-1),  # even
-    #         (0, start[1]),  # even
-    #         (len(grid)-1, start[1]),  # even
-    #         # TODO probably also need to include corners
-    #     ]
-    # }
     result = 0
-    # number of grids that are fully flooded (with odd number of steps)
-    # assumption: grid size is also odd, and start (S) is in the center
-
-    def sequence(n):
-        # 1, 5, 13, 25, 41, 61, ...
-        return 2*(n+1)**2 - 2*(n+1) + 1
-
-    # max distance to any fully flooded sub-grid
-    # fully flooded meaning that all odd-reachable nodes are in range
+    # max distance to any full sub-grid
+    # full meaning that all odd/even reachable nodes are in range
     dist_to_last = n_steps // len(grid) - 1
-    fully_flooded_grids = sequence(dist_to_last)
-    # print(fully_flooded_grids * len(grid))
-    # TODO WIP
     if dist_to_last % 2 == 0:
         even = (dist_to_last+1)**2
         odd = dist_to_last**2
     else:
         even = dist_to_last**2
         odd = (dist_to_last+1)**2
-    result += odd * bfs(grid, start, task=2, odd=True)[0]
-    result += even * bfs(grid, start, task=2, odd=False)[0]
-    # now located in the center of the last fully flooded grid
+    result += even * bfs(grid, start, task=2, odd=True)[0]
+    result += odd * bfs(grid, start, task=2, odd=False)[0]
+    # now located in the center of the last full grid
     remaining_steps = n_steps - dist_to_last * len(grid)
-    print(result)
-    print(remaining_steps, "remaining steps")
+    parity = bool(remaining_steps % 2)
     edge_starts = [
-        ((len(grid)-1, start[1]), remaining_steps-len(grid)//2-1, True),  # U
-        ((len(grid)-1, start[1]), remaining_steps-len(grid)-len(grid)//2-1, False),  # UU
-        ((0, start[1]), remaining_steps-len(grid)//2-1, True),  # D
-        ((0, start[1]), remaining_steps-len(grid)-len(grid)//2-1, False),  # DD
-        ((start[0], len(grid)-1), remaining_steps-len(grid)//2-1, True),  # L
-        ((start[0], len(grid)-1), remaining_steps-len(grid)-len(grid)//2-1, False),  # LL
-        ((start[0], 0), remaining_steps-len(grid)//2-1, True),  # R
-        ((start[0], 0), remaining_steps-len(grid)-len(grid)//2-1, False),  # RR
+        ((len(grid)-1, start[1]), remaining_steps-len(grid)//2-1, parity),  # U
+        ((len(grid)-1, start[1]), remaining_steps-len(grid)-len(grid)//2-1, not parity),  # UU
+        ((0, start[1]), remaining_steps-len(grid)//2-1, parity),  # D
+        ((0, start[1]), remaining_steps-len(grid)-len(grid)//2-1, not parity),  # DD
+        ((start[0], len(grid)-1), remaining_steps-len(grid)//2-1, parity),  # L
+        ((start[0], len(grid)-1), remaining_steps-len(grid)-len(grid)//2-1, not parity),  # LL
+        ((start[0], 0), remaining_steps-len(grid)//2-1, parity),  # R
+        ((start[0], 0), remaining_steps-len(grid)-len(grid)//2-1, not parity),  # RR
     ]
     for edge_start, steps, odd in edge_starts:
         result += bfs(grid, edge_start, task=2,
                       odd=odd, limit=steps)[0]
     corner_starts = [
-        ((0, 0), remaining_steps-1, dist_to_last, True),
-        ((0, 0), remaining_steps-len(grid)-1, dist_to_last+1, False),
-        ((0, len(grid)-1), remaining_steps-1, dist_to_last, True),
-        ((0, len(grid)-1), remaining_steps-len(grid)-1, dist_to_last+1, False),
-        ((len(grid)-1, 0), remaining_steps-1, dist_to_last, True),
-        ((len(grid)-1, 0), remaining_steps-len(grid)-1, dist_to_last+1, False),
-        ((len(grid)-1, len(grid)-1), remaining_steps-1, dist_to_last, True),
-        ((len(grid)-1, len(grid)-1), remaining_steps-len(grid)-1, dist_to_last+1, False),
+        ((0, 0), remaining_steps-1, dist_to_last, not parity),
+        ((0, 0), remaining_steps-len(grid)-1, dist_to_last+1, parity),
+        ((0, len(grid)-1), remaining_steps-1, dist_to_last, not parity),
+        ((0, len(grid)-1), remaining_steps-len(grid)-1, dist_to_last+1, parity),
+        ((len(grid)-1, 0), remaining_steps-1, dist_to_last, not parity),
+        ((len(grid)-1, 0), remaining_steps-len(grid)-1, dist_to_last+1, parity),
+        ((len(grid)-1, len(grid)-1), remaining_steps-1, dist_to_last, not parity),
+        ((len(grid)-1, len(grid)-1), remaining_steps-len(grid)-1, dist_to_last+1, parity),
     ]
     for corner_start, steps, reps, odd in corner_starts:
         reachable = bfs(grid, corner_start, task=2,
                         odd=odd, limit=steps)[0]
         result += reps * reachable
-    print(614864614526014)
     return result
 
 
 def run():
     grid = read_grid()
 
-    # print(bfs(grid, find_start(grid), task=1)[0])
+    print(bfs(grid, find_start(grid), task=1)[0])
     print(task2(grid))
-    614864614526014
 
-    """
-    1. calculate number of steps required to flood a single grid 
-    when entering with % 2 == 0/1, along with number of flooded tiles.
-    note: does it matter from where am I entering? probably not
-    2. start at grid coordinates (0, 0) and move by 1 to all sides with 
-    incremented steps counter and call recursively on neighbors, like a
-    grid-level BFS... do it until counter would exceed the limit
-    remember coordinates of visited grids
-    3. manually run local BFS here (on all grids on border)
-    
-    - this will not be enough though... some kind of jumping and caching
-    will be required 
-    
-    - try to calculate for each coordinate in a single grid the distances
-    to all sides    
-    
-     
-    """
 
 if __name__ == "__main__":
     run()
